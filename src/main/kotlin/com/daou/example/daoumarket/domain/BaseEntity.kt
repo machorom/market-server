3c4f858676a61d7d039ccb3bb751664824f18661
webmcr <webmcr@daou.co.kr>
2023-02-22 16:16:29 +0900

    Auditing 적용 jpa 변경시간 적용

--- src/main/kotlin/com/daou/example/daoumarket/domain/BaseEntity.kt
+++ src/main/kotlin/com/daou/example/daoumarket/domain/BaseEntity.kt
@@ -1,20 +1,30 @@
 package com.daou.example.daoumarket.domain
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
 import jakarta.persistence.*
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
 abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
 
     @CreatedDate
+    @JsonSerialize(using = LocalDateTimeSerializer::class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer::class)
     var createdAt: LocalDateTime = LocalDateTime.now()
 
     @LastModifiedDate
+    @JsonSerialize(using = LocalDateTimeSerializer::class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer::class)
     var updatedAt: LocalDateTime = LocalDateTime.now()
 
 }
